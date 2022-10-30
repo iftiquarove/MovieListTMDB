fe23f39759e20206f830d9e26804b14652fc123b
ultron33 <78212054+ultron33@users.noreply.github.com>
2022-10-30 22:03:36 +0600

    Initial Commit

---
+++ MovieListTMDB/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MovieListTMDB
+//
+//  Created by Iftiquar Ahmed Ove on 30/10/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
