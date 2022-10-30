3c9def7fda9ae22e5565a81a6c1bcad2e81534fe
ultron33 <78212054+ultron33@users.noreply.github.com>
2022-10-30 22:15:36 +0600

    Data Parsed from API

--- MovieListTMDB/Network Services/MovieWebServices.swift
+++ MovieListTMDB/Network Services/MovieWebServices.swift
@@ -5,3 +5,69 @@
 //  Created by Iftiquar Ahmed Ove on 30/10/22.
 //
 
+import Foundation
+
+class MovieWebServices {
+    static let shared = MovieWebServices()
+    let API_KEY = "139a42a64413eecc49f5e7925bdb3c1c"
+    
+    enum endpoints{
+        static let base = "https://api.themoviedb.org/3/"
+        static let posterBaseURL = "https://image.tmdb.org/t/p/w200"
+        
+        case getMovieList
+        
+        var string_value: String{
+            switch self {
+            case .getMovieList:
+                return endpoints.base + "search/movie"
+            }
+        }
+        
+        var url: URL{
+            return URL(string: string_value) ?? URL(string: "")!
+        }
+    }
+    
+    // MARK: - Functions
+    
+    // *************************** Get Movie List ************************
+    // -----------------------------------------------------------------------
+    
+    /// Get Photos by page
+    /// - Parameters:
+    ///   - page: the page number  from the api of what the movie list will be given
+    ///   - queryWord: the kwyeord through which the user will search for movies
+    func getMovieList(page: Int, queryWord: String, completion: @escaping(_ movieList: MovieList?, _ error: String?)-> Void){
+        if Utility.isConnectedToNetwork(){
+            guard var urlComponents = URLComponents(string: endpoints.getMovieList.url.absoluteString) else {return}
+            urlComponents.queryItems = [
+                URLQueryItem(name: "api_key", value: API_KEY),
+                URLQueryItem(name: "query", value: queryWord),
+                URLQueryItem(name: "page", value: "\(page)"),
+            ]
+            guard let url = urlComponents.url else {return}
+            URLSession.shared.dataTask(with: url) { (data, response, error) in
+                if error != nil{
+                    let error = error?.localizedDescription ?? "not found"
+                    print("🔴 error in parsing Data: ", error)
+                    completion(nil, error)
+                    return
+                }
+                guard let data = data else { return}
+                do {
+                    let parsedMovieList = try JSONDecoder().decode(MovieList.self, from: data)
+                    completion(parsedMovieList, nil)
+                    
+                } catch {
+                    print("🔴 error in decoding Data: ", error)
+                    completion(nil, error.localizedDescription)
+                    return
+                }
+            }.resume()
+        }else{
+            completion(nil, "No Network")
+        }
+    }
+}
+
